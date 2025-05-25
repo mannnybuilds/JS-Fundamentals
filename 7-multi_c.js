464bb8320b1353aca69618f27571de99e7be9b30
emmanuel <mannnybuilds@gmail.com>
2025-05-25 23:05:08 +0100

    Add multi argument file

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const { argv } = require('node:process');
+
+const x = Number(argv[2]);
+
+if (!Number.isInteger(x) || x < 0) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
