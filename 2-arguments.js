fe429e462bfce62013bb6f5c81347c9f3545a7aa
emmanuel <mannnybuilds@gmail.com>
2025-05-25 22:44:35 +0100

    Add multi argument file

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,7 +3,7 @@ const args = process.args
 
 if (args === 0) {
   console.log("No argument");
-} else if (args === 1) {
+} else if (args.length === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
